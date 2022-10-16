296e022415a5a034d0caa1241991fbaeb612b1b8
Kirill Allyanov <kir.allyanov@gmail.com>
2022-10-16 19:02:15 +0300

    array  created

---
+++ Final.cs
@@ -0,0 +1,21 @@
+﻿
+string [] array = {"Python", "2", "int", "3", "world", "hello"};
+
+
+SrotArray(array);
+
+void SrotArray(string[] array)
+{
+    string[] newarr = new string[array.Length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length<=3)
+        {
+            newarr[i]=array[i];;
+            Console.Write($"{newarr[i]}" + ", ");
+        }
+    }
+}
+
+
+
