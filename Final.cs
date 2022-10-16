d01bdfb4dcb80caafcf50804fc1e203e21f92d43
Kirill Allyanov <kir.allyanov@gmail.com>
2022-10-16 19:02:53 +0300

    method named

--- Final.cs
+++ Final.cs
@@ -2,19 +2,11 @@
 string [] array = {"Python", "2", "int", "3", "world", "hello"};
 
 
-SrotArray(array);
+SortArray(array);
 
-void SrotArray(string[] array)
+void SortArray(string[] array)
 {
-    string[] newarr = new string[array.Length];
-    for(int i = 0; i < array.Length; i++)
-    {
-        if(array[i].Length<=3)
-        {
-            newarr[i]=array[i];;
-            Console.Write($"{newarr[i]}" + ", ");
-        }
-    }
+    
 }
 
 
